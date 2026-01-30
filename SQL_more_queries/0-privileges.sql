a557e0ef766ab37eb4083cf23dc61877831c2cba
Emilio Revueltas <emilio.revueltas@gmail.com>
2026-01-29 20:55:57 -0600

    update

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,3 +1,7 @@
---Show grants
+-- Configuración de usuarios y listado de privilegios
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost';
+GRANT SELECT ON *.* TO 'user_0d_2'@'localhost';
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
