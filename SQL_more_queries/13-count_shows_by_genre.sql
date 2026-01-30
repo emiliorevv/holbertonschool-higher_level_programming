474c355276a6a9a0ff981f002f449bc093cbde94
Emilio Revueltas <emilio.revueltas@gmail.com>
2026-01-29 21:49:47 -0600

    update

---
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,8 @@
+-- Lists all genres and the number of shows linked to each
+-- Displays: genre - number_of_shows
+-- Results are sorted by number_of_shows in descending order
+SELECT tv_genres.name AS genre, COUNT(tv_show_genres.show_id) AS number_of_shows
+FROM tv_genres
+         JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+GROUP BY tv_genres.name
+ORDER BY number_of_shows DESC;
