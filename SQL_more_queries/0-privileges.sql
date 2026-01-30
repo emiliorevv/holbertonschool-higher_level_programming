c2bbe491398336e0f637ac186ccbb2ccdf37cbf3
Emilio Revueltas <emilio.revueltas@gmail.com>
2026-01-29 21:53:01 -0600

    update

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,4 +1,4 @@
--- Configuración de usuarios y listado de privilegios
---de usuarios 1 y 2 en `localhost`.
+-- Lists all privileges of the MySQL Users `user_0d_1`
+-- and `user_0d_2` in `localhost`.
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
